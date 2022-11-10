9b8792061f90f65307cf351e5d49d45eb6582e04
chuciy <103220191+chuciy@users.noreply.github.com>
2022-11-10 01:22:17 -0800

    stage2

--- Assets/Scripts/Main_Controll.cs
+++ Assets/Scripts/Main_Controll.cs
@@ -5,24 +5,41 @@ using UnityEngine;
 public class Main_Controll : MonoBehaviour
 {
     public List<Camera> cameras;
+
     public GameObject upper_box;
+    public GameObject trees;
+    public GameObject outter_particle;
     private bool _ceiling;
+
+    public GameObject dir_light;
+    public float daylight_time;
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
         cameras[0].enabled = true;
         _ceiling = true;
+        timer = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= daylight_time) 
+        {
+            timer -= daylight_time;
+        }
+        //dir_light.transform.Rotate(Vector3.right * 360 * Time.deltaTime / daylight_time);
+        dir_light.transform.eulerAngles = Vector3.right * 360 * timer / daylight_time;
+
     }
 
     public void Toggle_ceiling() 
     {
         _ceiling =  !_ceiling;
-        upper_box.SetActive(_ceiling);
+        upper_box.SetActive(_ceiling); 
+        trees.SetActive(!_ceiling);
+        outter_particle.SetActive(!_ceiling);
     }
 }
